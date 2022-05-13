33aa35180e8ec08e2598ac4e8964fc47089c798c
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2022-05-12 20:54:32 -0500

    Update runp.sh

--- image/runp.sh
+++ image/runp.sh
@@ -7,7 +7,7 @@ docker run \
   --mount type=tmpfs,destination=/root/app/ \
   --shm-size=256m \
   -p $((5900+$1)):5900 \
-  -e VNC_SERVER_PASSWORD=pwpwpw \
+  -e VNC_SERVER_PASSWORD=${PASS} \
   -e GO_SITE=$2 \
   --name chrome-ct-$1 \
   --user apps \
